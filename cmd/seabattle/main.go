c8dd45c81d7ae277c0f1d664afaaf44a4674028b
Oleg Alexandrov <teratronsirimus@gmail.com>
2021-02-08 19:31:31 +0200

    page.html

--- cmd/seabattle/main.go
+++ cmd/seabattle/main.go
@@ -10,8 +10,10 @@ import (
 )
 
 type page struct {
-	Lang  string
-	Title string
+	Name   string
+	Lang   string
+	Title  string
+	Author string
 }
 
 func init() {
@@ -19,6 +21,31 @@ func init() {
 	if err != nil {
 		log.Println(err)
 	}
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	home := &page{
+		Name:   "Sea Battle",
+		Lang:   "en",
+		Title:  "Sea Battle",
+		Author: "Oleg Alexandrov",
+	}
+	tmpl, err := template.ParseFiles("template/page.html",
+		"template/index.html",
+		"template/header.html",
+		"template/footer.html")
+	if err == nil {
+		err = tmpl.ExecuteTemplate(w, "page", home)
+	}
+	if err != nil {
+		//w.WriteHeader(http.StatusInternalServerError)
+		_, _ = fmt.Fprintf(w, err.Error())
+	}
+
 }
 
 func main() {
@@ -35,27 +62,4 @@ func main() {
 	log.Printf("Listening on port %s", port)
 	log.Printf("Open http://localhost:%s in the browser", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
-}
-
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-	page := &page{
-		Lang:  "en",
-		Title: "Sea Battle",
-	}
-	tmpl, err := template.ParseFiles("template/page.html",
-		"template/index.html",
-		"template/header.html",
-		"template/footer.html")
-	if err == nil {
-		err = tmpl.ExecuteTemplate(w, "page", page)
-	}
-	if err != nil {
-		//w.WriteHeader(http.StatusInternalServerError)
-		_, _ = fmt.Fprintf(w, err.Error())
-	}
-
 }
